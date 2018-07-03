fd0e3133f2f847498831f45019efca7b3fa0c90c
huylm1604 <39546666+huylm1604@users.noreply.github.com>
2018-07-03 14:48:21 +0700

    Add Manage Room

---
+++ Project_TouchCinema/Admin/ManageRoom.aspx
@@ -0,0 +1,48 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Layout/AdminLayout.Master" AutoEventWireup="true" CodeBehind="ManageRoom.aspx.cs" Inherits="Project_TouchCinema.ManageRoom" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPage" runat="server">
+    <h1>Cinema Room Management</h1>
+            
+        &nbsp;<asp:Label ID="Label1" runat="server" Text="Search by RoomID:"></asp:Label>
+    <asp:TextBox ID="txtSearch" runat="server"></asp:TextBox>
+&nbsp;&nbsp;&nbsp;
+    <asp:Button ID="btnSearch" runat="server" Text="Search" CssClass="button" OnClick="btnSearch_Click"/>
+&nbsp;&nbsp;&nbsp;
+    <asp:Button ID="btnShowAll" runat="server" Text="Show All Records" CssClass="button" OnClick="btnShowAll_Click"/>
+    <br />
+    
+        <br />
+    <asp:Label ID="lblMessage" runat="server" Text=" "></asp:Label>
+        <br />
+    <asp:Button ID="btnUpdateActive" runat="server" Text="Update Activation" CssClass="button" Width="200px" OnClick="btnUpdateActive_Click" />
+        <br />
+        
+        <asp:GridView ID="gvStaffList" runat="server" AutoGenerateColumns="False" BackColor="White" BorderColor="#E7E7FF" BorderStyle="None" BorderWidth="1px" CellPadding="3" GridLines="Horizontal">
+            <AlternatingRowStyle BackColor="#F7F7F7" />
+            <Columns>
+                <asp:BoundField DataField="RoomID" HeaderText="RoomID" >
+                <ItemStyle HorizontalAlign="Center" />
+                </asp:BoundField>
+                <asp:BoundField DataField="NumberOfSeat" HeaderText="NumberOfSeat" >
+                <ItemStyle HorizontalAlign="Center" />
+                </asp:BoundField>
+                <asp:TemplateField HeaderText="IsAvailable">
+                    <ItemTemplate>
+                        <asp:CheckBox ID="isActive" runat="server" Checked='<%# Eval("IsActive").ToString().Equals("True")  %>'/>
+                    </ItemTemplate>
+                    <ItemStyle HorizontalAlign="Center" />
+                </asp:TemplateField>
+            </Columns>
+            <FooterStyle BackColor="#B5C7DE" ForeColor="#4A3C8C" />
+            <HeaderStyle BackColor="#4A3C8C" Font-Bold="True" ForeColor="#F7F7F7" />
+            <PagerStyle BackColor="#E7E7FF" ForeColor="#4A3C8C" HorizontalAlign="Right" />
+            <RowStyle BackColor="#E7E7FF" ForeColor="#4A3C8C" />
+            <SelectedRowStyle BackColor="#738A9C" Font-Bold="True" ForeColor="#F7F7F7" />
+            <SortedAscendingCellStyle BackColor="#F4F4FD" />
+            <SortedAscendingHeaderStyle BackColor="#5A4C9D" />
+            <SortedDescendingCellStyle BackColor="#D8D8F0" />
+            <SortedDescendingHeaderStyle BackColor="#3E3277" />
+        </asp:GridView>
+        
+</asp:Content>
---
+++ Project_TouchCinema/Admin/ManageRoom.aspx.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using RoomLibrary;
+using System.Drawing;
+
+namespace Project_TouchCinema
+{
+    public partial class ManageRoom : System.Web.UI.Page
+    {
+        List<RoomDTO> AdminRoomList = new List<RoomDTO>();
+        RoomDAO dao = new RoomDAO();
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            
+
+            if (!IsPostBack)
+            {
+                AdminRoomList = dao.GetRoomList();
+                Session.Add("AdminRoomList", AdminRoomList);
+                gvStaffList.DataSource = AdminRoomList;
+                gvStaffList.DataBind();
+
+            }
+        }
+
+        protected void btnSearch_Click(object sender, EventArgs e)
+        {
+            List<RoomDTO> list = (List<RoomDTO>)Session["AdminRoomList"];
+            int searchValue = 0;
+            try
+            {
+                searchValue = Convert.ToInt32(txtSearch.Text);
+            }
+            catch
+            {
+                lblMessage.Text = "Room ID must be a number! Please type again!";
+                lblMessage.ForeColor = Color.Red;
+                return;
+            }
+            if (!searchValue.Equals(""))
+            {
+                RoomDTO searchResult = SearchInListByID(list, searchValue);
+                if (searchResult!=null)
+                {
+                    List<RoomDTO> roomSearchList = new List<RoomDTO>();
+                    roomSearchList.Add(searchResult);
+                    lblMessage.Text = "";
+                    gvStaffList.Visible = true;
+                    gvStaffList.DataSource = null;
+                    gvStaffList.DataSource = roomSearchList;
+                    gvStaffList.DataBind();
+                }
+                else
+                {
+                    gvStaffList.DataSource = null;
+                    gvStaffList.Visible = false;
+                    lblMessage.Text = "No record found!";
+                    lblMessage.ForeColor = Color.Red;
+                }
+            }
+        }
+
+        protected void btnShowAll_Click(object sender, EventArgs e)
+        {
+            lblMessage.Text = "";
+            gvStaffList.Visible = true;
+            List<RoomDTO> list = (List<RoomDTO>)Session["AdminRoomList"];
+            gvStaffList.DataSource = list;
+            gvStaffList.DataBind();
+        }
+
+        protected void btnUpdateActive_Click(object sender, EventArgs e)
+        {
+            List<RoomDTO> list = (List<RoomDTO>)Session["AdminRoomList"];
+            foreach (GridViewRow row in gvStaffList.Rows)
+            {
+                CheckBox status = (row.Cells[2].FindControl("isActive") as CheckBox);
+                int id = Convert.ToInt32(row.Cells[0].Text);
+                if (status.Checked)
+                {
+                    if (dao.UpdateRoomStatus(id, 1))
+                    {
+                        for (int i = 0; i <= list.Count - 1; i++)
+                        {
+                            if (list[i].RoomID == id)
+                            {
+                                list[i].IsActive = true;
+                            }
+                        }
+                    }
+                }
+                else
+                {
+                    if (dao.UpdateRoomStatus(id, 0))
+                    {
+                        for (int i = 0; i <= list.Count - 1; i++)
+                        {
+                            if (list[i].RoomID == id)
+                            {
+                                list[i].IsActive = false;
+                            }
+                        }
+                    }
+                }
+            }
+            gvStaffList.DataSource = list;
+            gvStaffList.DataBind();
+            lblMessage.Text = "Successfully updated";
+            lblMessage.ForeColor = Color.Green;
+        }
+
+        public RoomDTO SearchInListByID(List<RoomDTO> list, int id)
+        {
+            RoomDTO result = new RoomDTO();
+            foreach (RoomDTO item in list)
+            {
+                if (item.RoomID == id)
+                {
+                    result = new RoomDTO
+                    {
+                        RoomID = item.RoomID,
+                        NumberOfSeat = item.NumberOfSeat,
+                        IsActive = item.IsActive
+                    };
+                }
+
+            }
+
+            return result;
+        }
+    }
+}
