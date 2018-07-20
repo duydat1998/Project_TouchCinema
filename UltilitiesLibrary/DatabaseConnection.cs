1b354e89e89c37760ef85ffb02d9641859d22b40
duydat1998 <32428939+duydat1998@users.noreply.github.com>
2018-07-20 16:32:28 +0700

    dat sua loi chinh ta

--- UltilitiesLibrary/DatabaseConnection.cs
+++ UltilitiesLibrary/DatabaseConnection.cs
@@ -15,31 +15,10 @@ namespace UltilitiesLibrary
 
         }
 
-        //Nhớ xóa MayHieuBT để về lại ConnectionString cũ
-        //ai dùng máy HieuBTSE62797 nhớ để thêm MayHieuBT
-        //Trước khi push nhớ comment all cho ai cần làm
         public string GetConnection()
         {
-            //return ConfigurationManager.ConnectionStrings["TouchCinemaDB"].ConnectionString;
-            return ConfigurationManager.ConnectionStrings["TouchCinemaDBMayHieuBT"].ConnectionString;
-            //private SqlConnection checkConn;
-            //strConnection = ConfigurationManager.ConnectionStrings["TouchCinemaDBMayHieuBT"].ConnectionString;
-            //try
-            //{
-            //    checkConn = new SqlConnection(strConnection);
-            //    checkConn.Open();
-            //}
-            //catch(Exception)
-            //{
-            //    strConnection = ConfigurationManager.ConnectionStrings["TouchCinemaDBMayHieuBT"].ConnectionString;
-            //}
-            //finally
-            //{
-            //    if(checkConn.State != ConnectionState.Closed)
-            //    {
-            //        checkConn.Close();
-            //    }
-            //}
+            return ConfigurationManager.ConnectionStrings["TouchCinemaDB"].ConnectionString;
+            //return ConfigurationManager.ConnectionStrings["TouchCinemaDBMayHieuBT"].ConnectionString;
         }
         
     }
